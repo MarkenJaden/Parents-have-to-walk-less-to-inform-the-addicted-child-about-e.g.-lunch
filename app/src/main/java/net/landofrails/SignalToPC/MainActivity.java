e10d446abc4a8af9d01f1fb8f923a1c806fea80f
MarkenJaden <jjsch1410@gmail.com>
2020-06-30 20:59:14 +0200

    Current state of development

---
+++ app/src/main/java/net/landofrails/SignalToPC/MainActivity.java
@@ -0,0 +1,96 @@
+package net.landofrails.SignalToPC;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Girish Bhalerao on 5/4/2017.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewReplyFromServer;
+    private EditText mEditTextSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSend = (Button) findViewById(R.id.btn_send);
+        Button buttonFood = (Button) findViewById(R.id.btn_essen);
+
+        mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
+        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
+
+        buttonSend.setOnClickListener(this);
+        buttonFood.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_send:
+                sendMessage(mEditTextSendMessage.getText().toString());
+                break;
+            case R.id.btn_essen:
+                sendMessage("essen");
+                break;
+        }
+    }
+
+    private void sendMessage(final String msg) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Socket s = new Socket("116.202.236.57", 1002);
+
+                    OutputStream out = s.getOutputStream();
+
+                    PrintWriter output = new PrintWriter(out);
+
+                    output.println(msg);
+                    output.flush();
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    final String st = input.readLine();
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String s = mTextViewReplyFromServer.getText().toString();
+                            if (st.trim().length() != 0)
+                                mTextViewReplyFromServer.setText(s + "\nFrom Server : " + st);
+                        }
+                    });
+
+                    output.close();
+                    out.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
